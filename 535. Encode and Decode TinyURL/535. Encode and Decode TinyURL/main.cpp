a2092ccd74b97f72c2ebfeb3001649fd230bffcb
mihuaz2Z <mihuayouxi@163.com>
2017-07-05 13:38:00 +0800

    add description

---
+++ 535. Encode and Decode TinyURL/535. Encode and Decode TinyURL/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  535. Encode and Decode TinyURL
+//
+//  Created by Ruiwen Feng on 2017/7/5.
+//  Copyright © 2017年 Ruiwen Feng. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
